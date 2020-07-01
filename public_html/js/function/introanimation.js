be38900ac5d0308d2be4fd45803ae89c0888a9fe
MiPc <MiPc@10.0.0.73>
2020-07-01 03:56:51 -0500

    Merge origin/master
    Conflicts:
    	js/man_movement.js
    	squaredip.php

--- public_html/js/function/introanimation.js
+++ public_html/js/function/introanimation.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+var typewriting;
 var txtcount = 0;
 var introtxt ="A long time in the future, in a faraway city-town... an office worker is stuck in her office. She is just tired of working and working in this grimmy building and just wants to get out " +
              "and scream ALELUYA! with all her might. Despite being stuck in such a tiny office, her employer came to work by Copter today! So she decides to take the chance and sneak into her " +
@@ -24,6 +24,7 @@ function skipintrou() {
     document.getElementById("human2").style.visibility="hidden";
     document.getElementById("skipinbut").style.visibility="hidden";
     document.getElementById("introtext").style.visibility="hidden";
+     typewriting.pause()
 }
 
 
@@ -33,7 +34,7 @@ function intro() {
     if(!skipintro) setTimeout(function(){ document.getElementById("introbuttons").style.visibility = "visible";document.getElementById("skipinbut").style.visibility="hidden"; }, 22000);
     
     song = "audio/typewriter-1.mp3";
-    var typewriting = new Audio(song);
+    typewriting = new Audio(song);
     typewriting.play();
     setTimeout(function(){ typewriting.pause(); }, 18000);
 }
